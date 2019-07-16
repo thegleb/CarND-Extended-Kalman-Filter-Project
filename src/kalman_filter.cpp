5e46f31744a03bbedb53af373ca0d98c0bc9bca1
thegleb <thegleb@users.noreply.github.com>
2019-07-15 23:42:11 -0700

    Fix files

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,10 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+MatrixXd I = MatrixXd::Identity(2, 2);
+VectorXd u = VectorXd(2);
+Tools tools = Tools();
+
 /* 
  * Please note that the Eigen library does not initialize 
  *   VectorXd or MatrixXd objects with zeros upon creation.
@@ -23,19 +27,49 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  MatrixXd Ft = F_.transpose();
+  // no motion ... for now
+  u << 0, 0;
+
+  x_ = F_ * x_ + u;
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  MatrixXd Hj = tools.CalculateJacobian(x_);
+  VectorXd h_x = VectorXd(3);
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  float px_2_py_2 = pow(px, 2) + pow(py, 2);
+  h_x <<
+    sqrt(px_2_py_2),
+    atan(py / px),
+    (px * vx + py * vy) / sqrt(px_2_py_2);
+  
+  VectorXd y = z - h_x;
+  MatrixXd Ht = Hj.transpose();
+  MatrixXd S = Hj * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  
+  // new state
+  x_ = x_ + (K * y);
+  P_ = (I - K * Hj) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,14 +11,52 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    std::cout << "The estimation vector must be non-zero and the same size as the ground truth vector.";
+    return rmse;
+  }
+
+  // TODO: accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // mean
+  rmse = rmse / estimations.size();
+
+  // rmse = square root of mean
+  return rmse.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  MatrixXd Hj(3,4);
+
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  
+  // check division by zero
+  if (px*px + py*py < 0.001) {
+    std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+    return Hj;
+  }
+
+  float px_2 = pow(px, 2);
+  float py_2 = pow(py, 2);
+  float px_2_py_2 = px_2 + py_2;
+  // compute the Jacobian matrix
+  Hj << px / sqrt(px_2_py_2), py / sqrt(px_2_py_2), 0, 0,
+    -py / (px_2_py_2), px / (px_2_py_2), 0, 0,
+    py * (vx * py - vy * px) / pow(px_2_py_2, 3 / 2),  px * (vy * px - vx * py) / pow(px_2_py_2, 3 / 2), px / sqrt(px_2_py_2), py / sqrt(px_2_py_2);
+
+  return Hj;
 }
